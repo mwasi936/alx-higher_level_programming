7f9e2fda9a78391a908179f65ac54a42a6635d2f
mwasi936 <mwasijeff936@gmail.com>
2024-03-20 02:08:53 -0700

    To uppercase

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def print_last_digit(number):
+    last_digit = abs(number) % 10
+    return last_digit
+
+# Test cases
+print(print_last_digit(98), end='')
+print(print_last_digit(0), end='')
+print(print_last_digit(-1024), end='')
