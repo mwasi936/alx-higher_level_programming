81cad17d4bc84def62b7ddf9f8241fad137b439d
mwasi936 <mwasijeff936@gmail.com>
2024-03-20 04:15:22 -0700

    a ^ b

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
 
 def pow(a, b):
+    
     result = 1
 
+    
     if b < 0:
         a = 1 / a
         b = -b
+
     
     for _ in range(b):
         result *= a
+
     
+    result = round(result, 10)
+
     return result
