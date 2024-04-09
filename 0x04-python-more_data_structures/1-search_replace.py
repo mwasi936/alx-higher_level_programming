9263ed2add0915042d8b97951abfb8c1083455d0
mwasi936 <mwasi936@gmail.com>
2024-04-09 13:19:10 +0000

    Search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def search_replace(my_list, search, replace):
     new_list = []
     for item in my_list:
@@ -8,9 +7,3 @@ def search_replace(my_list, search, replace):
         else:
             new_list.append(item)
     return new_list
-
-my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
-new_list = search_replace(my_list, 2, 89)
-
-print(new_list)
-print(my_list)
